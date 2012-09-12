ae6b0ec563d32560871aceb437c1f1bc0d96dabf
Brian Mannakee <bmannakee@gmail.com>
2012-09-11 20:20:01 -0700

    Revert "Revert "Revert "code to begin conditioning the two datasets for merge"""
    This reverts commit c48c5820fd1733465f1f9e06297eb5425730d941.

--- life_expectancy.R
+++ life_expectancy.R
@@ -2,19 +2,6 @@ load_le <- function(){
   le <- read.csv("~/Stat571A/IHME_US_COUNTY_LIFE_EXPECTANCY_1987_2009.csv")
   colnames(le) <- c('fips','State','County','Year','Male','Female','White.Male',
                'White.Female','Black.Male','Black.Female','Male.Delta','Female.Delta')
-  # Econ datat in est09ALL.txt is fixed width
-  col_widths <- c(2,4,9,9,9,5,5,5,9,9,9,5,5,5,9,9,9,5,5,5,7,7,7,8,8,8,5,5,5,46,3,23)
-  col_names <- c('State.FIPS','County.FIPS','all.ages.pov.percent','all.ages.pov.lower',
-                 'all.ages.pov.upper','median.household.income.est','income.lower','income.upper',
-                 'county.name','postal.code','date.time')
-  econ_data <- read.fwf('est09ALL.txt',col_widths)
-  econ_data <- econ_data[,c(1,2,6:8,21:23,30:32)]
-  names(econ_data) <- col_names
-  library(reshape)
-  state_len <- nchar(econ_data$State.FIPS)
-  county_len <- nchar(econ_data$County.FIPS)
-  zeros <- c('0','00','000','0000')
-  econ_data$State.FIPS <- paste(econ_data$State.FIPS,zeros[5-state_len-county_len],sep='')
-  return(list(l=le,e=econ_data))
+  return(le)
 }
 
