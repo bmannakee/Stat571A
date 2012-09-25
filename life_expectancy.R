686ba2d03b1747c0ffa850c0da3b51fd3f6964be
Brian Mannakee <bmannakee@gmail.com>
2012-09-24 21:42:50 -0700

    revisions to life_expectancy to get the income.delta correct, and deal with some NA's. Most NA's are left in. ggplot will deal with them. Changes to the presentation, continuing to add stuff.

--- life_expectancy.R
+++ life_expectancy.R
@@ -4,19 +4,36 @@ load_le <- function(){
                 'White.Female','Black.Male','Black.Female','Male.Delta','Female.Delta')
      # Econ datat in est09ALL.txt is fixed width	
      col_widths <- c(2,4,9,9,9,5,5,5,9,9,9,5,5,5,9,9,9,5,5,5,7,7,7,8,8,8,5,5,5,46,3,23)
-     col_names <- c('State.FIPS','County.FIPS','all.ages.pov.percent','all.ages.pov.lower',
-                    'all.ages.pov.upper','median.household.income.est','income.lower','income.upper',
+     col_names <- c('State.FIPS','County.FIPS','all.ages.pov.percent.09','all.ages.pov.lower.09',
+                    'all.ages.pov.upper.09','median.household.income.est.09','income.lower.09','income.upper.09',
                     'county.name','postal.code','date.time')
-     econ_data <- read.fwf('est09ALL.txt',col_widths)
-     econ_data_89 <- read.fwf('est89all.txt',col_widths)
+     col_names_89 <- c('State.FIPS','County.FIPS','all.ages.pov.percent.89','all.ages.pov.lower.89',
+                    'all.ages.pov.upper.89','median.household.income.est.89','income.lower.89','income.upper.89')
+     
+     econ_data <- read.fwf('est09ALL.txt',col_widths,stringsAsFactors=FALSE)
+     econ_data_89 <- read.fwf('est89all.txt',col_widths,stringsAsFactors=FALSE)
      econ_data <- econ_data[,c(1,2,6:8,21:23,30:32)]
+     econ_data_89 <- econ_data_89[,c(1,2,6:8,21:23)]
+     #econ_data_89[,3:8] <- sapply(econ_data_89[,3:8],as.numeric)
      names(econ_data) <- col_names
-     state_len <- nchar(econ_data$State.FIPS)	
+     names(econ_data_89) <- col_names_89
+     state_len <- nchar(econ_data$State.FIPS)
+     state_len_89 <- nchar(econ_data_89$State.FIPS)
      county_len <- nchar(econ_data$County.FIPS)
+     county_len_89 <- nchar(econ_data_89$County.FIPS)
      zeros <- c('','0','00','000')
      econ_data$County.FIPS <- paste(zeros[4-county_len],econ_data$County.FIPS,sep='')
      econ_data$fips <- paste(econ_data$State.FIPS,econ_data$County.FIPS,sep='')
-     total_frame <- merge(le,econ_data,by='fips')
+     econ_data_89$County.FIPS <- paste(zeros[4-county_len_89],econ_data_89$County.FIPS,sep='')
+     econ_data_89$fips <- paste(econ_data_89$State.FIPS,econ_data_89$County.FIPS,sep='')
+     econ_data_89 <- econ_data_89[,3:9]
+     econ_data <- subset(econ_data,fips!='15005')
+     econ_data_89 <- subset(econ_data_89,fips!='15005')
+     econ_data[,3:8] <- sapply(econ_data[,3:8],as.numeric)
+     econ_data_89[,1:5] <- sapply(econ_data_89[,1:5],as.numeric)
+     econ_frame <- merge(econ_data,econ_data_89,by='fips')
+     econ_frame$Income.delta <- econ_frame$median.household.income.est.09 - econ_frame$median.household.income.est.89
+     total_frame <- merge(le,econ_frame,by='fips')
      stopifnot(nchar(total_frame$fips)==5 || nchar(total_frame$fips)==4)
      return(total_frame)
  }
